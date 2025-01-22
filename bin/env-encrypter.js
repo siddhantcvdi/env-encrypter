a55757d399502e2890f2874d850eb8c82bf3e373
siddhantcvdi <siddhantchaturvedi2004@gmail.com>
2025-01-23 02:16:23 +0530

    Added watch feature

--- bin/env-encrypter.js
+++ bin/env-encrypter.js
@@ -1,35 +1,62 @@
 #!/usr/bin/env node
-import { program } from 'commander';
-import { encryptFile, decryptFile } from '../src/encryptDecrypt.js';
-import { findEnvFile, findDotLockFile} from '../src/utils.js';
+import { program } from "commander";
+import { encryptFile, decryptFile } from "../src/encryptDecrypt.js";
+import { findEnvFile, findDotLockFile } from "../src/utils.js";
+import chokidar from "chokidar";
+import chalk from 'chalk';
 
 program
-  .command('encrypt')
-  .description('Encrypt a .env file')
-  .option('-p, --password <password>', 'Password for encryption')
+  .command("encrypt")
+  .description("Encrypt a .env file")
+  .option("-p, --password <password>", "Password for encryption")
   .action(async (options) => {
-    const envFile = findEnvFile(); 
+    const envFile = findEnvFile();
     if (!envFile) {
-      console.error('Error: .env file not found in the current directory.');
-      process.exit(1);  
+      console.error("Error: .env file not found in the current directory.");
+      process.exit(1);
     }
     const password = options.password;
     await encryptFile(envFile, password);
   });
 
 program
-  .command('decrypt')
-  .description('Decrypt an encrypted .env file')
-  .option('-p, --password <password>', 'Password for decryption')
+  .command("decrypt")
+  .description("Decrypt an encrypted .env file")
+  .option("-p, --password <password>", "Password for decryption")
   .action(async (options) => {
-    const envFile = findDotLockFile();  
+    const envFile = findDotLockFile();
     if (!envFile) {
-      console.error('Error: dotlock file not found in the current directory.');
-      process.exit(1);  
+      console.error("Error: dotlock file not found in the current directory.");
+      process.exit(1);
     }
     const password = options.password;
     await decryptFile(envFile, password);
   });
 
-program.parse(process.argv);
+program
+  .command("watch")
+  .description("Automatically encrypt .env file on changes")
+  .option("-p, --password <password>", "Password for encryption")
+  .action((options) => {
+    const envFile = findEnvFile();
+    if (!envFile) {
+      console.error("Error: .env file not found in the current directory.");
+      process.exit(1);
+    }
+    const password = options.password;
+    console.clear();
+    console.log(chalk.green(`\nWatching for changes in ${envFile}...`));
+    const watcher = chokidar.watch(envFile, { persistent: true });
 
+    watcher.on("change", async () => {
+      console.log(`\nDetected change in ${envFile}. \nEncrypting...`);
+      await encryptFile(envFile, password);
+      console.log(chalk.green(`\nWatching for changes in ${envFile}...`));
+    });
+
+    watcher.on("error", (error) => {
+      console.error("Watcher error:", error);
+    });
+  });
+
+program.parse(process.argv);
